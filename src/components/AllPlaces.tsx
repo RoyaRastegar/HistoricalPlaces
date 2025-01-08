98adefeb940c1ba30dded4d5b3f7fcdaabf0d775
Roya Rastegar <roya.rastegar54196@gmail.com>
2025-01-08 09:14:22 +0800

    add getdata inside Allplaces component

--- src/components/AllPlaces.tsx
+++ src/components/AllPlaces.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchHistoricalPlaces } from '../lib/redux/historicalPlacesSlice/historicalplacesSlice.ts';
+import { AppDispatch, RootState } from '../lib/redux/store.ts';
+import { useDispatch, useSelector } from 'react-redux';
 
 const AllPlaces = () => {
+  // #regionconst
+  const dispatch: AppDispatch = useDispatch();
+  const { places } = useSelector((state: RootState) => state.historicalPlaces);
+  // #endregionconst
+
+  // #regionhooks
+  useEffect(() => {
+    dispatch(fetchHistoricalPlaces());
+  }, [dispatch]);
+  // #endregionhooks
+  console.log(places);
   return <div>all places</div>;
 };
 
