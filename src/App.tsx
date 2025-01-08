618e64958be1e06a11335ec81b2b463930b0da05
Roya Rastegar <roya.rastegar54196@gmail.com>
2025-01-08 16:31:57 +0800

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import './App.css';
 import AllPlaces from './components/AllPlaces.tsx';
 import { Provider } from 'react-redux';
 import store from './lib/redux/store.ts';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PlaceDetail from './components/PlaceDetail.tsx';
 function App() {
   return (
     <Provider store={store}>
-      <AllPlaces />
+      <Router>
+        <Routes>
+          <Route path='/' element={<AllPlaces />} />
+          <Route path='/placedetails/:id' element={<PlaceDetail />} />
+        </Routes>
+      </Router>
     </Provider>
   );
 }
